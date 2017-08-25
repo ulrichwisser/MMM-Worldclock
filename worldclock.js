8e5e8029ba8c3163efb7bab762894cff412e80a2
eouia <eouia0819@gmail.com>
2017-08-25 10:58:57 +0200

    clean tabs

--- worldclock.js
+++ worldclock.js
@@ -5,155 +5,155 @@
 * By eouia
 */
 Module.register("worldclock",{
-	// Module config defaults.
-	defaults: {
-		timeFormat: 'LT', //defined in moment.js format()
-		style: 'left', //where the time could be located; 'top', 'left','right','bottom'
-		clocks: [
-			{
-				title: "Seoul",
-				timezone:"Asia/Seoul",
-				flag:"kr"
-			},
-		]
-	},
-	// Define required scripts.
-	getScripts: function() {
-		return ["moment.js", "moment-timezone.js"]
-	},
-	// Define styles.
-	getStyles: function() {
-		return ["worldclock.css"]
-	},
+  // Module config defaults.
+  defaults: {
+    timeFormat: 'LT', //defined in moment.js format()
+    style: 'left', //where the time could be located; 'top', 'left','right','bottom'
+    clocks: [
+      {
+        title: "Seoul",
+        timezone:"Asia/Seoul",
+        flag:"kr"
+      },
+    ]
+  },
+  // Define required scripts.
+  getScripts: function() {
+    return ["moment.js", "moment-timezone.js"]
+  },
+  // Define styles.
+  getStyles: function() {
+    return ["worldclock.css"]
+  },
 
-	getCommands: function() {
-		return [
-			{
-		    command: 'worldclock',
-		    description: 'Tell the time of worldclock',
-		    callback : 'TLGBOT_worldclock',
-		  },
-		]
-	},
+  getCommands: function() {
+    return [
+      {
+        command: 'worldclock',
+        description: 'Tell the time of worldclock',
+        callback : 'TLGBOT_worldclock',
+      },
+    ]
+  },
 
-	TLGBOT_worldclock: function(command, handler) {
-		var text = ""
-		var m = moment()
-		for (var c in this.config.clocks) {
-			var clock = this.config.clocks[c]
-			var title = (clock.title) ? clock.title : clock.timezone
-			if (clock.timezone) {
-				m.tz(clock.timezone)
-			} else {
-				m.local()
-			}
+  TLGBOT_worldclock: function(command, handler) {
+    var text = ""
+    var m = moment()
+    for (var c in this.config.clocks) {
+      var clock = this.config.clocks[c]
+      var title = (clock.title) ? clock.title : clock.timezone
+      if (clock.timezone) {
+        m.tz(clock.timezone)
+      } else {
+        m.local()
+      }
 
-			text += "`" + m.format(this.config.timeFormat) + "`"
-			text += " in *" + title + "*\n"
-		}
-		text = (text) ? text : "I cannot answer now, sorry."
-		handler.reply("TEXT", text, {parse_mode:'Markdown'})
-	},
+      text += "`" + m.format(this.config.timeFormat) + "`"
+      text += " in *" + title + "*\n"
+    }
+    text = (text) ? text : "I cannot answer now, sorry."
+    handler.reply("TEXT", text, {parse_mode:'Markdown'})
+  },
 
-	start: function() {
+  start: function() {
 
-		this.loadCSS()
-		var self = this
-		setInterval(function() {
-			self.updateDom()
-		}, 1000)
+    this.loadCSS()
+    var self = this
+    setInterval(function() {
+      self.updateDom()
+    }, 1000)
 
-		// Set locale.
-		moment.locale(config.language)
-	},
+    // Set locale.
+    moment.locale(config.language)
+  },
 
-	clockFormat: function(c, index) {
-		var worldWrapper = document.createElement("div")
-		worldWrapper.className = "world world-" + index
+  clockFormat: function(c, index) {
+    var worldWrapper = document.createElement("div")
+    worldWrapper.className = "world world-" + index
 
-		var clock = moment()
+    var clock = moment()
 
-		if (c.timezone == null || undefined ) {
-			clock.local()
-		} else {
-			clock.tz(c.timezone)
-		}
+    if (c.timezone == null || undefined ) {
+      clock.local()
+    } else {
+      clock.tz(c.timezone)
+    }
 
-		var timeString
-		timeString = clock.format(this.config.timeFormat)
+    var timeString
+    timeString = clock.format(this.config.timeFormat)
 
-		var timeWrapper = document.createElement("div")
-		timeWrapper.innerHTML = timeString
-		timeWrapper.className = "time"
-		//timeWrapper.className = "time bright medium"
+    var timeWrapper = document.createElement("div")
+    timeWrapper.innerHTML = timeString
+    timeWrapper.className = "time"
+    //timeWrapper.className = "time bright medium"
 
-		var captionWrapper = document.createElement("div")
-		captionWrapper.className = "caption"
-		//captionWrapper.className = 'caption small normal'
+    var captionWrapper = document.createElement("div")
+    captionWrapper.className = "caption"
+    //captionWrapper.className = 'caption small normal'
 
-		var zoneWrapper = document.createElement("div")
-		zoneWrapper.className = 'zone'
+    var zoneWrapper = document.createElement("div")
+    zoneWrapper.className = 'zone'
 
-		if (c.title != null) {
-			zoneWrapper.innerHTML = c.title
-		} else {
-			zoneWrapper.innerHTML = c.timezone
-		}
+    if (c.title != null) {
+      zoneWrapper.innerHTML = c.title
+    } else {
+      zoneWrapper.innerHTML = c.timezone
+    }
 
-		captionWrapper.appendChild(zoneWrapper)
+    captionWrapper.appendChild(zoneWrapper)
 
-		var gapWrapper = document.createElement("div")
-		gapWrapper.className = 'gap'
-		gapWrapper.innerHTML = 'UTC ' + clock.format('Z')
+    var gapWrapper = document.createElement("div")
+    gapWrapper.className = 'gap'
+    gapWrapper.innerHTML = 'UTC ' + clock.format('Z')
 
-		captionWrapper.appendChild(gapWrapper)
-
-
-
-		var mainWrapper = document.createElement("div")
-		mainWrapper.className = "main"
-
-		if (c.flag) {
-			var flagWrapper = document.createElement("div")
-			flagWrapper.className = "flag"
-			var flagIconWrapper = document.createElement("span")
-			flagIconWrapper.className = "flag-icon flag-icon-squared"
-			flagIconWrapper.className += " flag-icon-" + c.flag
-			flagWrapper.appendChild(flagIconWrapper)
-			mainWrapper.appendChild(flagWrapper)
-		}
+    captionWrapper.appendChild(gapWrapper)
 
 
 
-		mainWrapper.appendChild(timeWrapper)
+    var mainWrapper = document.createElement("div")
+    mainWrapper.className = "main"
 
-		worldWrapper.appendChild(mainWrapper)
-		worldWrapper.appendChild(captionWrapper)
-
-		return worldWrapper
-	},
-
-	notificationReceived: function(noti, payload) {
-
-	},
-
-
+    if (c.flag) {
+      var flagWrapper = document.createElement("div")
+      flagWrapper.className = "flag"
+      var flagIconWrapper = document.createElement("span")
+      flagIconWrapper.className = "flag-icon flag-icon-squared"
+      flagIconWrapper.className += " flag-icon-" + c.flag
+      flagWrapper.appendChild(flagIconWrapper)
+      mainWrapper.appendChild(flagWrapper)
+    }
 
 
-	// Override dom generator.
-	getDom: function() {
-		var wrapper = document.createElement("div")
-		wrapper.className
-			= "worldtime"
-				+ ' style-' + this.config.style
-		var c
-		for (c in this.config.clocks) {
-			wrapper.appendChild(this.clockFormat(this.config.clocks[c], c))
-		}
-		return wrapper
-	},
 
-	loadCSS: function() {
+    mainWrapper.appendChild(timeWrapper)
+
+    worldWrapper.appendChild(mainWrapper)
+    worldWrapper.appendChild(captionWrapper)
+
+    return worldWrapper
+  },
+
+  notificationReceived: function(noti, payload) {
+
+  },
+
+
+
+
+  // Override dom generator.
+  getDom: function() {
+    var wrapper = document.createElement("div")
+    wrapper.className
+      = "worldtime"
+        + ' style-' + this.config.style
+    var c
+    for (c in this.config.clocks) {
+      wrapper.appendChild(this.clockFormat(this.config.clocks[c], c))
+    }
+    return wrapper
+  },
+
+  loadCSS: function() {
     var css = [
       {
         id:'flag-icon-CSS',
