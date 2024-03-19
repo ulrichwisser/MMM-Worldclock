8d2f218b8426795654665f6ee53e85271f0946ea
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2024-03-20 00:12:17 +0100

    Format

--- MMM-Worldclock.js
+++ MMM-Worldclock.js
@@ -1,68 +1,72 @@
 /* global Log, Module, moment, config */
-/* MagicMirror²
-* Module: worldlock
-*
-* By eouia
-*/
-Module.register("MMM-Worldclock",{
+/*
+ * MagicMirror²
+ * Module: worldlock
+ *
+ * By eouia
+ */
+Module.register("MMM-Worldclock", {
   // Module config defaults.
   defaults: {
-    timeFormat: 'LT', //defined in moment.js format()
-    style: 'left', //where the time could be located; 'top', 'left','right','bottom'
+    timeFormat: "LT", // defined in moment.js format()
+    style: "left", // where the time could be located; 'top', 'left','right','bottom'
     offsetTimezone: null, // or "Europe/Berlin" to get difference from this timezone to each clock.
     clocks: [
       {
         title: "Seoul",
-        timezone:"Asia/Seoul",
-        flag:"kr",
-      },
+        timezone: "Asia/Seoul",
+        flag: "kr"
+      }
     ]
   },
   // Define required scripts.
-  getScripts: function() {
+  getScripts () {
     return ["moment.js", "moment-timezone.js"]
   },
   // Define styles.
-  getStyles: function() {
+  getStyles () {
     return ["MMM-Worldclock.css"]
   },
 
-  getCommands: function(commander) {
-    if (commander.constructor.name == 'TelegramBotCommandRegister') {
+  getCommands (commander) {
+    if (commander.constructor.name === "TelegramBotCommandRegister") {
       return [
         {
-          command: 'worldclock',
-          description: 'Tell the time of worldclock',
-          callback : 'TLGBOT_worldclock',
-        },
+          command: "worldclock",
+          description: "Tell the time of worldclock",
+          callback: "TLGBOT_worldclock"
+        }
       ]
     }
   },
 
-  TLGBOT_worldclock: function(command, handler) {
-    var text = ""
-    var m = moment()
-    for (var c in this.config.clocks) {
-      var clock = this.config.clocks[c]
-      var title = (clock.title) ? clock.title : clock.timezone
+  TLGBOT_worldclock (command, handler) {
+    let text = ""
+    const m = moment()
+    for (const c in this.config.clocks) {
+      const clock = this.config.clocks[c]
+      const title = clock.title
+        ? clock.title
+        : clock.timezone
       if (clock.timezone) {
         m.tz(clock.timezone)
       } else {
         m.local()
       }
 
-      text += "`" + m.format(this.config.timeFormat) + "`"
-      text += " in *" + title + "*\n"
+      text += `\`${m.format(this.config.timeFormat)}\``
+      text += ` in *${title}*\n`
     }
-    text = (text) ? text : "I cannot answer now, sorry."
-    handler.reply("TEXT", text, {parse_mode:'Markdown'})
+    text = text
+      ? text
+      : "I cannot answer now, sorry."
+    handler.reply("TEXT", text, {parse_mode: "Markdown"})
   },
 
-  start: function() {
-
+  start () {
     this.loadCSS()
-    var self = this
-    setInterval(function() {
+    const self = this
+    setInterval(() => {
       self.updateDom()
     }, 1000)
 
@@ -70,39 +74,39 @@ Module.register("MMM-Worldclock",{
     moment.locale(config.language)
   },
 
-  clockFormat: function(c, index) {
-    var worldWrapper = document.createElement("div")
-    worldWrapper.className = "world world-" + index
+  clockFormat (c, index) {
+    const worldWrapper = document.createElement("div")
+    worldWrapper.className = `world world-${index}`
 
-    var clock = moment()
+    const clock = moment()
 
-    if (c.timezone == null || undefined ) {
+    if (c.timezone == null || undefined) {
       clock.local()
     } else {
       clock.tz(c.timezone)
     }
 
-    var timeFormat
-    if (c.timeFormat == null || undefined ) {
-      timeFormat= this.config.timeFormat
+    let timeFormat
+    if (c.timeFormat == null || undefined) {
+      timeFormat = this.config.timeFormat
     } else {
       timeFormat = c.timeFormat
     }
 
-    var timeString
+    let timeString
     timeString = clock.format(this.config.timeFormat)
 
-    var timeWrapper = document.createElement("div")
+    const timeWrapper = document.createElement("div")
     timeWrapper.innerHTML = timeString
     timeWrapper.className = "time"
-    //timeWrapper.className = "time bright medium"
+    // timeWrapper.className = "time bright medium"
 
-    var captionWrapper = document.createElement("div")
+    const captionWrapper = document.createElement("div")
     captionWrapper.className = "caption"
-    //captionWrapper.className = 'caption small normal'
+    // captionWrapper.className = 'caption small normal'
 
-    var zoneWrapper = document.createElement("div")
-    zoneWrapper.className = 'zone'
+    const zoneWrapper = document.createElement("div")
+    zoneWrapper.className = "zone"
 
     if (c.title != null) {
       zoneWrapper.innerHTML = c.title
@@ -112,44 +116,41 @@ Module.register("MMM-Worldclock",{
 
     captionWrapper.appendChild(zoneWrapper)
 
-    var gapWrapper = document.createElement("div")
-    gapWrapper.className = 'gap'
+    const gapWrapper = document.createElement("div")
+    gapWrapper.className = "gap"
 
-    var gap = ""
+    let gap = ""
     if (this.config.offsetTimezone) {
-      var ori = "+"
-      var oclock = moment().tz(this.config.offsetTimezone)
-      var os = clock.utcOffset() - oclock.utcOffset()
+      let ori = "+"
+      const oclock = moment().tz(this.config.offsetTimezone)
+      let os = clock.utcOffset() - oclock.utcOffset()
       if (os < 0) {
         os = oclock.utcOffset() - clock.utcOffset()
         ori = "-"
       }
 
-      var dur = moment.duration(os, 'minutes')
-      gap = ori + moment.utc(dur.asMilliseconds()).format('HH:mm')
+      const dur = moment.duration(os, "minutes")
+      gap = ori + moment.utc(dur.asMilliseconds()).format("HH:mm")
     } else {
-      gap = "UTC " + clock.format('Z')
+      gap = `UTC ${clock.format("Z")}`
     }
 
     gapWrapper.innerHTML = gap
 
     captionWrapper.appendChild(gapWrapper)
 
-
-
-    var mainWrapper = document.createElement("div")
+    const mainWrapper = document.createElement("div")
     mainWrapper.className = "main"
 
     if (c.flag) {
-      var flagWrapper = document.createElement("div")
+      const flagWrapper = document.createElement("div")
       flagWrapper.className = "flag"
-      var flagIconWrapper = document.createElement("span")
+      const flagIconWrapper = document.createElement("span")
       flagIconWrapper.className = "flag-icon flag-icon-squared"
-      flagIconWrapper.className += " flag-icon-" + c.flag
+      flagIconWrapper.className += ` flag-icon-${c.flag}`
       flagWrapper.appendChild(flagIconWrapper)
       mainWrapper.appendChild(flagWrapper)
     }
-
 
 
     mainWrapper.appendChild(timeWrapper)
@@ -160,45 +161,42 @@ Module.register("MMM-Worldclock",{
     return worldWrapper
   },
 
-  notificationReceived: function(noti, payload) {
+  notificationReceived (noti, payload) {
 
   },
 
 
-
-
   // Override dom generator.
-  getDom: function() {
-    var wrapper = document.createElement("div")
-    wrapper.className
-      = "worldtime"
-        + ' style-' + this.config.style
-    var c
+  getDom () {
+    const wrapper = document.createElement("div")
+    wrapper.className =
+      "worldtime" +
+      ` style-${this.config.style}`
+    let c
     for (c in this.config.clocks) {
       wrapper.appendChild(this.clockFormat(this.config.clocks[c], c))
     }
     return wrapper
   },
 
-  loadCSS: function() {
-    var css = [
+  loadCSS () {
+    const css = [
       {
-        id:'flag-icon-CSS',
-        href: 'https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/2.8.0/css/flag-icon.min.css'
+        id: "flag-icon-CSS",
+        href: "https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/2.8.0/css/flag-icon.min.css"
       }
     ]
-    css.forEach(function(c) {
-      if (!document.getElementById(c.id))
-      {
-        var head  = document.getElementsByTagName('head')[0]
-        var link  = document.createElement('link')
-        link.id   = c.id
-        link.rel  = 'stylesheet'
-        link.type = 'text/css'
+    css.forEach((c) => {
+      if (!document.getElementById(c.id)) {
+        const head = document.getElementsByTagName("head")[0]
+        const link = document.createElement("link")
+        link.id = c.id
+        link.rel = "stylesheet"
+        link.type = "text/css"
         link.href = c.href
-        link.media = 'all'
+        link.media = "all"
         head.appendChild(link)
       }
     })
-  },
+  }
 })
